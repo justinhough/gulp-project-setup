ecba11606508292bf0dc4ffce68a36bbe7098af3
Justin Hough <justinrayhough@gmail.com>
2015-09-05 15:24:48 -0700

    gulpfile and package added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,141 @@
-// Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp'),
+    pkg = require('./package.json'),
+    jekyll = require('gulp-jekyll-stream'),
+    connect = require('gulp-connect'),
+    git = require('gulp-git'),
+    bump = require('gulp-bump'),
+    filter = require('gulp-filter'),
+    tag_version = require('gulp-tag-version'),
+    del = require('del');
+
 
 // basePaths
-var basePaths = {
-  src: '_src/',
-  dest: '_build/',
-  partials: '_partials/',
+var path = {
+  src: 'source/',
+  dest: 'dist/',
   centurion: './node_modules/centurion-framework/lib/sass'
 },
 paths = {
   scripts: {
-    src: basePaths.src + 'js/',
-    dest: basePaths.dest + 'js/'
+    src: path.src + 'js/',
+    dest: path.dest + 'js/'
   },
   styles: {
-    src: basePaths.src + 'sass/',
-    dest: basePaths.dest + 'css/'
+    src: path.src + 'sass/',
+    dest: path.dest + 'css/'
   },
   images: {
-    src: basePaths.src + 'images/',
-    dest: basePaths.dest + 'images/min/'
+    src: path.src + 'assets/',
+    dest: path.dest + 'assets/min/'
   }
 };
 
-
-// Include Our Plugins
-var //jshint = require('gulp-jshint'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    connect = require('gulp-connect'),
-    fileinclude = require('gulp-file-include')
-    del = require('del');
-
-
-// Clean Build
+// Clean Build Folder
+// ================================
 gulp.task('clean', function () {
-  del([ basePaths.dest ]);
+  del([ path.dest + '/**/*' ]);
 });
 
-
 // Connect
+// ================================
 gulp.task('connect', function() {
   connect.server({
-    root: basePaths.dest,
+    root: path.dest,
     port: '5316',
     livereload: true
   });
 });
- 
-gulp.task('html', function () {
-  gulp.src('./'+basePaths.dest+'/*.html');
+
+// Build Site using Jekyll
+// https://www.npmjs.com/package/gulp-jekyll-stream/
+// ================================
+gulp.task('jekyll', function() {
+  return gulp.src(process.cwd())
+    .pipe(jekyll({
+      bundleExec: true,
+      quiet: true,
+      safe: false,
+      cwd: process.cwd(),
+      layouts: '_layouts',
+      plugins: '_plugins',
+      source: path.src,
+      destination: path.dest
+    }));
 });
 
+// BUMP_VERSION
+// https://www.npmjs.com/package/gulp-tag-version/
+// ================================
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
 
-// HTML include
-gulp.task('fileinclude', function() {
-  gulp.src([ basePaths.src + '*.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: basePaths.partials
-    }))
-    .pipe(gulp.dest( basePaths.dest ));
-});
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+        .pipe(bump({type: importance}))
+        .pipe(gulp.dest('./'))
+        .pipe(git.commit('bumps package version'))
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        //.pipe(tag_version());
+};
 
 
-// Lint Task
-/*
-gulp.task('lint', function() {
-  return gulp.src( paths.scripts.src + '*.js' )
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-*/
 
-// Compile Our Sass
-gulp.task('sass', function() {
-  return gulp.src( paths.styles.src + '*.scss')
-    .pipe(sourcemaps.init())
-      //.pipe(sass())
-      .pipe(sass({includePaths: [basePaths.centurion], errLogToConsole: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest( paths.styles.dest ));
-});
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-  return gulp.src( paths.scripts.src + '*.js' )
-    .pipe(concat( 'all.js' ))
-    .pipe(gulp.dest( paths.scripts.dest ))
-    .pipe(rename( 'all.min.js' ))
-    .pipe(uglify())
-    .pipe(gulp.dest( paths.scripts.dest ));
-});
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch( paths.scripts.src + '*.js', [/* 'lint', */ 'scripts']);
-    gulp.watch( paths.styles.src + '*.scss', ['sass']);
-    gulp.watch(['./'+basePaths.dest+'/*.html'], ['html']);
-    gulp.watch( basePaths.src+'/*.html', ['fileinclude']);
-});
 
-// Default Task
-gulp.task('default', [ 'clean', 'connect', /* 'lint', */ 'sass', 'scripts', 'fileinclude', 'watch' ]);
+
+// TASKS
+// ================================
+gulp.task('default', [ 'clean', 'jekyll', 'connect' ]);
+
+// bump versions
+gulp.task('patch', function() { return inc('patch'); })
+gulp.task('feature', function() { return inc('minor'); })
+gulp.task('release', function() { return inc('major'); })
+
+// Things to Do:
+// ===================================
+// @TODO: [x] clean
+// @TODO: [x] connect (server)
+// @TODO: [x] jekyll
+
+// @TODO: [] bump verion number
+
+// @TODO: [] sass
+// @TODO: [] include Centurion framework at core for usage
+// @TODO: [] uncss
+// @TODO: [] postcss
+// @TODO: [] critical (create a critical CSS file)
+// @TODO: [] add header to CSS files
+
+// @TODO: [] uglify
+// @TODO: [] concat
+// @TODO: [] add header to JS files
+
+// @TODO: [] imagemin
+// @TODO: [] svgmin
+// @TODO: [] responsive_images
+
+// @TODO: [] HTML inject CSS and JS into templates
+
+// @TODO: [] copy any other files
+
+// @TODO: [] watch for changed files and run necessary processes
+// @TODO: [] Livereload
+
+// @TODO: [] CHANGELOG generate
+// @TODO: [] gh-pages deploy
+// @TODO: [] create ZIP file of version
