b883f0b0c13f0675b7d120939340950503b01f2c
Justin Hough <justinrayhough@gmail.com>
2016-05-28 00:21:14 -0700

    JS and Sass moved to `assets/`

---
+++ source/assets/js/script.js
@@ -0,0 +1,12 @@
+//= require "../../../bower_components/lazysizes/plugins/noscript/ls.noscript.min.js"
+//= require "../../../bower_components/lazysizes/plugins/respimg/ls.respimg.min.js"
+//= require "../../../bower_components/lazysizes/lazysizes.min.js"
+//= require "../../../bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js"
+//= require "../../../bower_components/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js"
+
+var testing = 0;
+
+function test() {
+  testing = 2;
+  console.log(testing);
+}
